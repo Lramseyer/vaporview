9e19bee01a34e51772a5af7bf74af1db980be8ab
Lloyd Ramseyer <laramseyer@gmail.com>
2025-11-08 19:32:02 -0800

    Fix rendering glitch on windows

--- src/webview/renderer.ts
+++ src/webview/renderer.ts
@@ -231,6 +231,7 @@ export const multiBitWaveformRenderer: WaveformRenderer = {
     ctx.globalAlpha = 1;
     ctx.transform(viewportSpecs.zoomRatio, 0, 0, viewportSpecs.zoomRatio, 0, 0);
     ctx.beginPath();
+    ctx.moveTo(-viewportSpecs.pixelTime * 2, 0);
     points.forEach(([x, y]) => {ctx.lineTo(x, y);});
     endPoints.reverse().forEach(([x, y]) => {ctx.lineTo(x, y);});
 
